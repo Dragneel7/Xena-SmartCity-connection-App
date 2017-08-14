38d06fa2cc66d9faf7d814253f7952dd75135528
surya saini <sainisurya.1@gmail.com>
2017-08-14 21:48:39 +0530

    login added

--- xena/views.py
+++ xena/views.py
@@ -1,34 +1,43 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
-from django.contrib.auth import login,logout
+from django.contrib.auth import login,logout,authenticate
 from django.core.urlresolvers import reverse
-
-def home(request):
-	return render(request,'xena/login.html')
+from django.contrib.auth.models import User
 
 def log_in(request):
 	form = AuthenticationForm()
 	if request.method == "POST":
 		form  = AuthenticationForm(data = request.POST)
 		if form.is_valid():
-			login(request,form.get_user())
-			return redirect('home')
+			username = form.get_user()
+			login(request,username)
+			print type(username)
+			return redirect('xena:home')
 	else:
 		return render(request,'xena/login.html',{'form':form})
 
 def sign_up(request):
-	print "hello"
+	
 	form = UserCreationForm()
 	if request.method == "POST":
 		form = UserCreationForm(data = request.POST)
 		if form.is_valid():
-			print "hello"
-			form.save()
 			
-			return redirect('home',request)
+			form.save()
+			user = User.objects.get(username = form.cleaned_data.get('username'))
+			print type(user)
+			user.backend='django.contrib.auth.backends.ModelBackend'
+			login(request,user)
+			return redirect('xena:home')
 		else:
 			print form.errors
 	else:
 		return render(request,'xena/signup.html',{'form':form})
+
+def home(request):
+	print request.user.username
+	username = request.user.username
+	return render(request,'xena/home.html',{'username':username})
+
 
 # Create your views here.
